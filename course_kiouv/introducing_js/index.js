4da4ecd53a1140497635fc0b9bbd6b2cfb484c45
Cedric Magnin <cedric.magnin@codeandtheory.com>
2018-09-12 16:26:13 +0100

    done

---
+++ course_kiouv/introducing_js/index.js
@@ -0,0 +1,13 @@
+var names = ['Frodo','Sam','Merry','Pippin'];
+// for (var i = 0; i<names.length; i++) {
+//   console.log(names[i]);
+// }
+var i = 0;
+// do {
+//   console.log(names[i]);
+//   i++;
+// } while (i < names.length)
+
+while (i< 0) {
+  console.log(names[i]);
+}
